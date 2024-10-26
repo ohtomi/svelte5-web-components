7cf5186e36e45d92b3167697a2d091978899af10
ohtomi <ohtomi.kenichi@gmail.com>
2024-10-26 10:27:11 +0900

    Tickerの内部実装をsetIntervalからsvelte/motionへ入れ替えた

--- src/lib/state/ticker.svelte.ts
+++ src/lib/state/ticker.svelte.ts
@@ -1,36 +1,60 @@
+import type {Unsubscriber} from 'svelte/store';
+import {type Tweened, tweened} from 'svelte/motion';
+
 declare const Ticker: unique symbol;
 
 type Ticker = {
     delay: number;
+    timer: Tweened<number>;
     isStarted: boolean;
-    intervalId: number | undefined;
+    unsubscribe: Unsubscriber | undefined;
     [Ticker]: never;
 };
 
 export const newTicker = (delay: number): Ticker => {
+    const timer = tweened(0, {duration: delay});
+
     const ticker = $state({
         delay,
+        timer,
         isStarted: false,
-        intervalId: undefined,
+        unsubscribe: undefined,
     });
 
     return ticker as Ticker;
 };
 
-export const startTicker = (ticker: Ticker, func: () => void): void => {
-    stopTicker(ticker);
+export const startTicker = async (ticker: Ticker, func: () => void): Promise<void> => {
+    await stopTicker(ticker);
 
     ticker.isStarted = true;
-    ticker.intervalId = setInterval(func, ticker.delay);
+    ticker.unsubscribe = ticker.timer.subscribe(async (value) => {
+        if (value < ticker.delay) {
+            return;
+        }
+
+        func();
+
+        await ticker.timer.update(() => 0, {duration: 0});
+        await ticker.timer.set(ticker.delay);
+    });
+
+    await ticker.timer.update(() => 0, {duration: 0});
+    await ticker.timer.set(ticker.delay);
 };
 
-export const stopTicker = (ticker: Ticker): void => {
+export const stopTicker = async (ticker: Ticker): Promise<void> => {
     if (!ticker.isStarted) {
         return;
     }
+    if (!ticker.unsubscribe) {
+        return;
+    }
 
-    clearInterval(ticker.intervalId);
+    ticker.unsubscribe();
 
     ticker.isStarted = false;
-    ticker.intervalId = undefined;
+    ticker.unsubscribe = undefined;
+
+    await ticker.timer.update(() => 0, {duration: 0});
 };
